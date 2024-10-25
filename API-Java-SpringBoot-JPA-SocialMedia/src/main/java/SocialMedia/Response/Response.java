8134b0ffec9f0b58c90c77c4fff4a0a1f8a5707a
CAO816 <caothithuthuy00000@gmail.com>
2024-10-25 16:09:07 +0700

    Upload again

---
+++ API-Java-SpringBoot-JPA-SocialMedia/src/main/java/SocialMedia/Response/Response.java
@@ -0,0 +1,17 @@
+package SocialMedia.Response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Response {
+	private boolean success;
+	private String message;
+	private Object result;
+}
+
