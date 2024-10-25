8c65d1d3b3874383e1bbde91558b1de9e92b5031
CAO816 <caothithuthuy00000@gmail.com>
2024-10-25 15:54:44 +0700

    Delete

--- MobileAndroid-Java-SocialMedia/app/build/generated/ap_generated_sources/debug/out/HCMUTE/SocialMedia/BR.java
+++
@@ -1,5 +0,0 @@
-package HCMUTE.SocialMedia;
-
-public class BR {
-  public static final int _all = 0;
-}
--- MobileAndroid-Java-SocialMedia/app/build/generated/data_binding_trigger/debug/HCMUTE/SocialMedia/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package HCMUTE.SocialMedia;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
